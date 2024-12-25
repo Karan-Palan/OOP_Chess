50aae1441c57e3e06d7434c6840cb4f3b931a177
karan-palan <karanpalan007@gmail.com>
2024-12-26 00:30:12 +0530

    refactor[chessboard]: remove redundancy initalizing the peices

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -12,31 +12,28 @@ interface Peice {
 
 const peices: Peice[] = [];
 
+// For rendering other peices
+for (let p = 0; p < 2; p++) {
+  const type = p === 0 ? "b" : "w";
+  const y = p === 0 ? 7 : 0;
+  peices.push({ image: `assets/images/rook_${type}.png`, x: 0, y });
+  peices.push({ image: `assets/images/rook_${type}.png`, x: 7, y });
+  peices.push({ image: `assets/images/knight_${type}.png`, x: 1, y });
+  peices.push({ image: `assets/images/knight_${type}.png`, x: 6, y });
+  peices.push({ image: `assets/images/bishop_${type}.png`, x: 2, y });
+  peices.push({ image: `assets/images/bishop_${type}.png`, x: 5, y });
+  peices.push({ image: `assets/images/queen_${type}.png`, x: 3, y });
+  peices.push({ image: `assets/images/king_${type}.png`, x: 4, y });
+}
 // Rendering the Black peices
 for (let i = 0; i < 8; i++) {
   peices.push({ image: "assets/images/pawn_b.png", x: i, y: 6 });
 }
-peices.push({ image: "assets/images/rook_b.png", x: 0, y: 7 });
-peices.push({ image: "assets/images/rook_b.png", x: 7, y: 7 });
-peices.push({ image: "assets/images/knight_b.png", x: 1, y: 7 });
-peices.push({ image: "assets/images/knight_b.png", x: 6, y: 7 });
-peices.push({ image: "assets/images/bishop_b.png", x: 2, y: 7 });
-peices.push({ image: "assets/images/bishop_b.png", x: 5, y: 7 });
-peices.push({ image: "assets/images/queen_b.png", x: 3, y: 7 });
-peices.push({ image: "assets/images/king_b.png", x: 4, y: 7 });
 
 // Rendering the White peices
 for (let i = 0; i < 8; i++) {
   peices.push({ image: "assets/images/pawn_w.png", x: i, y: 1 });
 }
-peices.push({ image: "assets/images/rook_w.png", x: 0, y: 0 });
-peices.push({ image: "assets/images/rook_w.png", x: 7, y: 0 });
-peices.push({ image: "assets/images/knight_w.png", x: 1, y: 0 });
-peices.push({ image: "assets/images/knight_w.png", x: 6, y: 0 });
-peices.push({ image: "assets/images/bishop_w.png", x: 2, y: 0 });
-peices.push({ image: "assets/images/bishop_w.png", x: 5, y: 0 });
-peices.push({ image: "assets/images/queen_w.png", x: 3, y: 0 });
-peices.push({ image: "assets/images/king_w.png", x: 4, y: 0 });
 
 export default function ChessBoard() {
   let board = [];
